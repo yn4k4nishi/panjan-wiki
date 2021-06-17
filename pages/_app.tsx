a170551931ec790298b6e708755c4e61b40a7820
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-06-17 14:29:10 +0900

    :+1: Update : faviconの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/index.css'
 import Footer from '@/components/footer'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/logo.svg" />
+      </Head>
       <Component {...pageProps} />
       <Footer />
     </>
