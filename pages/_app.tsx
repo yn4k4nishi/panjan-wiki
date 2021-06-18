1cfaefa4cc24a822799568bad86080643cdf3546
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-06-19 05:22:51 +0900

    :rotating_light: Tests : ログインの実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Provider session={pageProps.session}>
         <Component {...pageProps} />
-        <Footer />
+        {/* <Footer /> */}
       </Provider>
     </>
   )
