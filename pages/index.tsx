f59cc51e4fb313d2f9f2194f618259c68e93736a
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-06-19 20:00:06 +0900

    :construction: WIP : ログイン画面を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,13 @@
-import Skeleton from 'react-loading-skeleton'
+import Image from 'next/image'
+import logo from '../public/logo.svg'
 
-import Nav from '@/components/nav'
-import Container from '@/components/container'
-import Entries from '@/components/entries'
-
-import { useEntries } from '@/lib/swr-hooks'
-
-export default function IndexPage() {
-  const { entries, isLoading } = useEntries()
-
-  if (isLoading) {
-    return (
-      <div>
-        <Nav />
-        <Container>
-          <Skeleton width={180} height={24} />
-          <Skeleton height={48} />
-          <div className="my-4" />
-          <Skeleton width={180} height={24} />
-          <Skeleton height={48} />
-          <div className="my-4" />
-          <Skeleton width={180} height={24} />
-          <Skeleton height={48} />
-        </Container>
-      </div>
+export default function Home() {
+    return(
+        <div className="w-screen h-screen">
+            <div className="">
+                <Image src={logo} width={400}></Image>
+                <h1>Panajan Wiki</h1>
+            </div>
+        </div>
     )
-  }
-
-  return (
-    <div>
-      <Nav />
-      <Container>
-        <Entries entries={entries} />
-      </Container>
-    </div>
-  )
 }
