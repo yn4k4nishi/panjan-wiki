a4bfadb0a817082ffcd53937c52dfdded7835763
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-06-26 16:43:27 +0900

    :+1: Update : DBに登録されてないメールアドレスは弾く

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { query } from '@/lib/db'
 
 const options = {
   providers: [
@@ -8,6 +9,21 @@ const options = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
+  callbacks: {
+    async signIn(profile){
+      const results = await query(
+        `
+        select mail from member where mail = ?
+        `,
+        profile.email
+      )
+      if ( results == '' ){
+        return false
+      } else {
+        return true
+      }
+    }
+  }
 }
 
 export default (req, res) => NextAuth(req, res, options)
