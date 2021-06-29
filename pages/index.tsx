c269e977d6e41b248fb0ec32e39a1608e247143f
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-06-29 15:30:54 +0900

    :rotating_light: Tests : loading画面の修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function IndexPage() {
     return (
       <div className="flex w-screen h-screen">
         <div className="mx-auto">
-            <Image src={logo} width={200}></Image>
+            <Image src={logo} width={200} className="items-center"></Image>
             <div className="flex flex-col items-center">
                 <p className="text-center text-6xl">Loading ...</p>
             </div>
